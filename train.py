bbaf14595c7f85d94e6b66a4b762e5346525aec2
anashussain <anas.hussain@youngs.com>
2026-06-24 14:16:21 +0500

    train 24 working good

--- train.py
+++ train.py
@@ -1,8 +1,12 @@
 from ultralytics import YOLO
 
 # model = YOLO("yolo11n.pt")
-model = YOLO("yolo11s.pt")
+# model = YOLO("yolo11s.pt")
 
+# Load a model
+model = YOLO("yolo26n.yaml")  # build a new model from YAML
+model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo26n.yaml").load("yolo26n.pt")  # build from YAML and transfer weights
 
 #model = YOLO("yolov8n.pt")  # small model for small dataset
 
